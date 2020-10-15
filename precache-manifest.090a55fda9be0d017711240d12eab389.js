2b56310ef07eb18ab5c63b42dd1b410062331ecf
Anthony Okwananke <anthonyokwananke@gmail.com>
2020-10-15 11:35:00 -0600

    Updates

---
+++ precache-manifest.090a55fda9be0d017711240d12eab389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725061e0c2c3b3d133cd3fe29eb86cc1",
+    "url": "/tony/index.html"
+  },
+  {
+    "revision": "9572ddcf438f1de00e00",
+    "url": "/tony/static/css/main.39aa7bf4.chunk.css"
+  },
+  {
+    "revision": "e18901d9012d22023e8d",
+    "url": "/tony/static/js/2.003a66f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tony/static/js/2.003a66f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9572ddcf438f1de00e00",
+    "url": "/tony/static/js/main.25302660.chunk.js"
+  },
+  {
+    "revision": "4fb61904041dafe98df7",
+    "url": "/tony/static/js/runtime-main.64db90f3.js"
+  }
+]);
